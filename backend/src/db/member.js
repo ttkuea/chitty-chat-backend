000e31e93acf927975745d45d32048eedfc11eba
Kueakun <turtlekungz@gmail.com>
2020-04-30 13:33:57 +0700

    remove unused member.js

--- backend/src/db/member.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
-const membersSchema = new Schema({
-    username: String,
-    joinedSince: Date,
-    lastRead: Date
-});
-module.exports = members = mongoose.model('members', membersSchema);
